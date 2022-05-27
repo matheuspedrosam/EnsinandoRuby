32130443c3435385ef657918dc7246d270c90818
matheuspedrosam <matheuspedrosa2002@gmail.com>
2022-05-26 21:37:08 -0300

    atualizando

--- 04 - Collections/2 - Map.rb
+++ 04 - Collections/2 - Map.rb
@@ -1,2 +1,34 @@
 #MAP ->
     #cria um array baseando-se em valores de outro array existente ou alterar os valores da original
+
+
+    #a introdoção já diz tudo, porém vou mostrar na prática.:
+
+    array = [1, 2, 3, 4]
+
+    novo_array = array.map do |a|   #perceba que eu estou atribuindo os novos valores a outra variavel.
+        a*2
+    end
+
+    puts novo_array # novo array com os valores antigos multiplicados por 2
+    puts '----'
+
+    #também existe a possibilidade de mudar os valores do array original.:
+
+    array1 = [1, 2, 3, 7]
+
+    array1.map! do |a|   # para fazer isso utiliza-se a exclamação(!) após o map
+        a*3
+    end
+
+    puts array1
+    puts '----'
+    #exemplos adicionais.:
+
+    array2 = ["matheus", "pedro", "joao"]
+
+    novo_array2 = array2.map do |nomes|
+        "seja bem vindo #{nomes}" << ", tudo bom?"
+    end
+
+    puts novo_array2
--- 04 - Collections/3 - Select.rb
+++ 04 - Collections/3 - Select.rb
@@ -1,2 +1,35 @@
 #SELECT ->
     #realiza uma seleção de elementos presentes em uma collection através de uma condição pré definida. Traz como resultado somente os valores que passam nesta condição.
+
+
+    #bom a introdução também já diz tudo, o select usamos para selecionar itens dentro de um array ou map
+    #de acordo com um condição, por exemplo, como se fosse uma filtragem.
+
+    #na pratica ->
+
+    array = [1, "matheus", 3, 7, 8, 22]
+    
+    novo_array = array.select do |valores|   #perceba que aqui eu também jogo os novos valores em outra variavel
+        valores % 2 == 0   #ou seja so vai entrar no novo array o valor for par
+    end
+    
+    puts novo_array
+    puts '----'
+
+    #com hash ->
+
+    hashzinho = {2 => "matheus", 3=> "joao pereira da silva", 7 => "pedro"}
+
+    novo_hash = hashzinho.select do |chave, valor|
+        chave > 2
+    end
+
+    puts novo_hash
+    puts '----'
+    #outros exemplos
+
+    novo_hash2 = hashzinho.select do |key, value|
+        value == "joao pereira da silva"
+    end
+
+    puts novo_hash2
